5b4a99cd912c952e3280c697a3c7d0a9b616f6df
FeeeeK <krmrclemens@gmail.com>
2020-04-09 13:56:54 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+import setuptools
+
+setuptools.setup(
+    name="MyQR",
+    version="2.3.1",
+    author="FeeeeK",
+    license="GPL-3.0",
+    url="https://github.com/FeeeeK/qrcode",
+    packages=setuptools.find_packages(),
+    install_requires=["imageio","numpy","Pillow"]
+)
